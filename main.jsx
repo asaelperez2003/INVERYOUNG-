3d0403fb599ebcc49313c1db01b7c9850205bc57
asaelperez2003 <148508895+asaelperez2003@users.noreply.github.com>
2026-08-06 21:02:15 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import InveryoungSimulator from "./Simulador.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <InveryoungSimulator />
+  </React.StrictMode>
+);
